46992416174efdd2a632cfdd96e9a6a4b98ee7e1
LucianoFraga27 <luciano.lucas@bllc.com.br>
2022-10-22 15:37:34 -0300

    Classe Campo - Método Adicionar Vizinhos

--- src/br/com/luciano/cm/modelo/Campo.java
+++ src/br/com/luciano/cm/modelo/Campo.java
@@ -5,21 +5,43 @@ import java.util.List;
 
 public class Campo {
 
-	private final int linha; 
+	private final int linha	; 							// [linha,coluna]
 	private final int coluna;
 	
-	private boolean aberto = false;  // Livre 
-	private boolean minado = false;  // Mina
-	private boolean marcado = false; // Bandeirinha
+	private boolean aberto 	= false;  					// Livre 
+	private boolean minado 	= false;  					// Mina
+	private boolean marcado = false; 					// Bandeirinha
 
-	private List<Campo> vizinhos = new ArrayList<>(); // Auto relacionamento 1,N
+	private List<Campo> vizinhos = new ArrayList<>(); 	// Auto relacionamento 1,N
 	
 	Campo(int linha, int coluna){
-		this.linha = linha;
+		this.linha 	= linha	;
 		this.coluna = coluna;
 	};
 	
-	
-	
-	
+	boolean adicionarVizinho(Campo vizinho) { 
+		
+		boolean linhaDiferente = this.linha != vizinho.linha   	;
+		boolean colunaDiferente = this.coluna != vizinho.coluna	;
+		boolean diagonal = linhaDiferente && colunaDiferente   	;
+		
+		int deltaLinha = Math.abs(this.linha - vizinho.linha)	;
+		int deltaColuna = Math.abs(this.coluna - vizinho.coluna);
+		int deltaGeral = deltaColuna + deltaLinha				;
+		
+		boolean caso = false;
+		
+		if(deltaGeral == 1 && !diagonal) {
+			vizinhos.add(vizinho);
+			caso = true;
+		}
+		else if (deltaGeral == 2 && diagonal) {
+			vizinhos.add(vizinho);
+			caso = true;
+		} else {
+			caso = false;
+		}	
+		return caso;
+	}
+
 }
