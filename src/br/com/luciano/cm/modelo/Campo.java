f0055214a2ef791d197fb5fd194b1d8164d30f9d
LucianoFraga27 <luciano.lucas@bllc.com.br>
2022-10-22 14:56:02 -0300

    Classe Campo - Atributos

--- src/br/com/luciano/cm/modelo/Campo.java
+++ src/br/com/luciano/cm/modelo/Campo.java
@@ -1,5 +1,25 @@
 package br.com.luciano.cm.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Campo {
 
+	private final int linha; 
+	private final int coluna;
+	
+	private boolean aberto = false;  // Livre 
+	private boolean minado = false;  // Mina
+	private boolean marcado = false; // Bandeirinha
+
+	private List<Campo> vizinhos = new ArrayList<>(); // Auto relacionamento 1,N
+	
+	Campo(int linha, int coluna){
+		this.linha = linha;
+		this.coluna = coluna;
+	};
+	
+	
+	
+	
 }
--- src/br/com/luciano/cm/modelo/Tabuleiro.java
+++ src/br/com/luciano/cm/modelo/Tabuleiro.java
@@ -2,4 +2,5 @@ package br.com.luciano.cm.modelo;
 
 public class Tabuleiro {
 
+	
 }
